7ec953f6cc866b89cbc8a19b4f418aa5bb593859
nepp-plus <nepp.plus@gmail.com>
2021-12-04 12:24:25 +0900

    두번째 프래그먼트 - 동작 관련 코드  - onActivityCreated / requireContext() 이용해서 작성

--- app/src/main/java/com/neppplus/fragment_20211204/fragments/SecondFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20211204/fragments/SecondFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20211204.R
+import kotlinx.android.synthetic.main.fragment_second.*
 
 class SecondFragment : Fragment() {
 
@@ -16,5 +18,17 @@ class SecondFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnToast.setOnClickListener {
+
+            Toast.makeText(requireContext(), "두번째 프래그먼트의 버튼 눌림", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
 
 }
